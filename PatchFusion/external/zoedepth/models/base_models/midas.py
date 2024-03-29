907c3bf860c2ef6ec0ee68746087c1418853ae47
Raj Shreyas <raj.shreyas922@gmail.com>
2024-03-29 14:35:22 -0700

    Added AdaIN to PatchFusion, Hardcoded image size for now.

--- PatchFusion/external/zoedepth/models/base_models/midas.py
+++ PatchFusion/external/zoedepth/models/base_models/midas.py
@@ -47,6 +47,7 @@ def get_activation(name, bank):
     return hook
 
 
+
 class Resize(object):
     """Resize sample to given size (width, height).
     """
@@ -172,6 +173,52 @@ class Resize(object):
         width, height = self.get_size(*x.shape[-2:][::-1])
         return nn.functional.interpolate(x, (int(height), int(width)), mode='bilinear', align_corners=True)
 
+class AdaIn(nn.Module):
+    def __init__(self, latent_size, out_channels):
+        super(AdaIn, self).__init__()
+        # self.mlp = EqualizedLinear(latent_size,
+        #                            channels * 2,
+        #                            gain=1.0, use_wscale=use_wscale)
+
+        self.mlp = nn.Sequential(
+                nn.Linear(latent_size, 512),
+                nn.LeakyReLU(),
+                nn.Linear(512, 256),
+                nn.LeakyReLU(),
+                nn.Linear(256, 128),
+                nn.LeakyReLU(),
+                nn.Linear(128, out_channels * 2))
+
+    def forward(self, x, latent, mean_shift=0.0, var_shift=0.0, scale=1.0):
+        style = self.mlp(latent)  # style => [batch_size, n_channels*2]
+        #print("ADAIN MLP SHAPE", style.shape) #torch.Size([1, 2048])
+        x = x.transpose(1, 2)  
+        #print("ADAIN SHAPE", x.shape) #torch.Size([1, 768, 1024])
+        shape = [-1, 2, x.size(1)] + (x.dim() - 2) * [1]
+        style = style.view(shape)  # [batch_size, 2, n_channels, ...]
+        #print("STYLE", style.shape)
+        mean = style[:, 1] - mean_shift
+        var = style[:, 0] + 1. - var_shift
+
+        #print("x shape:", x.shape)
+        #print("mean shape:", mean.shape)
+        #print("var shape:", var.shape)
+        #print("scale value:", scale)  # scale is a scalar, so we directly print its value
+
+        x = x * (var * scale) + mean
+        return x
+ 
+
+class Transpose(nn.Module):
+    def __init__(self, dim0, dim1):
+        super(Transpose, self).__init__()
+        self.dim0 = dim0
+        self.dim1 = dim1
+
+    def forward(self, x):
+        x = x.transpose(self.dim0, self.dim1)
+        return x
+    
 class PrepForMidas(object):
     def __init__(self, resize_mode="minimal", keep_aspect_ratio=True, img_size=384, do_resize=True):
         if isinstance(img_size, int):
@@ -185,7 +232,22 @@ class PrepForMidas(object):
     def __call__(self, x):
         return self.normalization(self.resizer(x))
 
+class ProjectReadout(nn.Module):
+    def __init__(self, in_features, start_index=1):
+        super(ProjectReadout, self).__init__()
+        self.start_index = start_index
 
+        self.project = nn.Sequential(nn.Linear(2 * in_features, in_features), nn.GELU())
+
+    def forward(self, x):
+        print("Project Readout", x.shape)
+        readout = x[:, 0].unsqueeze(1).expand_as(x[:, self.start_index:])
+        features = torch.cat((x[:, self.start_index:], readout), -1)
+        print("Project Readout, After Reshape", x.shape)
+        x = self.project(features)
+        print("Projection Shape", x.shape)
+        return x
+   
 class MidasCore(nn.Module):
     def __init__(self, midas, trainable=False, fetch_features=True, layer_names=('out_conv', 'l4_rn', 'r4', 'r3', 'r2', 'r1'), freeze_bn=False, keep_aspect_ratio=True,
                  img_size=384, **kwargs):
@@ -201,6 +263,8 @@ class MidasCore(nn.Module):
             img_size (int, tuple, optional): Input resolution. Defaults to 384.
         """
         super().__init__()
+        
+        
         self.core = midas
         self.output_channels = None
         self.core_out = {}
@@ -210,15 +274,96 @@ class MidasCore(nn.Module):
         self.handles = []
         # self.layer_names = ['out_conv','l4_rn', 'r4', 'r3', 'r2', 'r1']
         self.layer_names = layer_names
-
+        features=[256, 512, 1024, 1024]
+        readout_oper = [
+            ProjectReadout(in_features=1024, start_index=0) for out_feat in features
+        ]
         self.set_trainable(trainable)
         self.set_fetch_features(fetch_features)
 
         self.prep = PrepForMidas(keep_aspect_ratio=keep_aspect_ratio,
                                  img_size=img_size, do_resize=kwargs.get('do_resize', True))
-
+        self.adain = AdaIn(512,1024)
+        
         if freeze_bn:
             self.freeze_bn()
+        vit_features = 1024
+        size=[384, 512]
+        self.act_postprocess1 = nn.Sequential(
+            readout_oper[0],
+            Transpose(1, 2),
+            nn.Unflatten(2, torch.Size([size[0] // 16, size[1] // 16])),
+            nn.Conv2d(
+                in_channels=vit_features,
+                out_channels=features[0],
+                kernel_size=1,
+                stride=1,
+                padding=0,
+            ),
+            nn.ConvTranspose2d(
+                in_channels=features[0],
+                out_channels=features[0],
+                kernel_size=4,
+                stride=4,
+                padding=0,
+                bias=True,
+                dilation=1,
+                groups=1,
+                ),
+            )
+        self.act_postprocess2 = nn.Sequential(
+            readout_oper[1],
+            Transpose(1, 2),
+            nn.Unflatten(2, torch.Size([size[0] // 16, size[1] // 16])),
+            nn.Conv2d(
+                in_channels=vit_features,
+                out_channels=features[1],
+                kernel_size=1,
+                stride=1,
+                padding=0,
+            ),
+            nn.ConvTranspose2d(
+                in_channels=features[1],
+                out_channels=features[1],
+                kernel_size=2,
+                stride=2,
+                padding=0,
+                bias=True,
+                dilation=1,
+                groups=1,
+            ),
+        )
+        self.act_postprocess3 = nn.Sequential(
+            readout_oper[2],
+            Transpose(1, 2),
+            nn.Unflatten(2, torch.Size([size[0] // 16, size[1] // 16])),
+            nn.Conv2d(
+                in_channels=vit_features,
+                out_channels=features[2],
+                kernel_size=1,
+                stride=1,
+                padding=0,
+                ),
+            )
+        self.act_postprocess4 = nn.Sequential(
+            readout_oper[3],
+            Transpose(1, 2),
+            nn.Unflatten(2, torch.Size([size[0] // 16, size[1] // 16])),
+            nn.Conv2d(
+                in_channels=vit_features,
+                out_channels=features[3],
+                kernel_size=1,
+                stride=1,
+                padding=0,
+            ),
+            nn.Conv2d(
+                in_channels=features[3],
+                out_channels=features[3],
+                kernel_size=3,
+                stride=2,
+                padding=1,
+                ),
+            )
 
     def set_trainable(self, trainable):
         self.trainable = trainable
@@ -254,23 +399,219 @@ class MidasCore(nn.Module):
             if isinstance(m, nn.BatchNorm2d):
                 m.eval()
         return self
+    
 
     def forward(self, x, denorm=False, return_rel_depth=False):
-        with torch.no_grad():
-            if denorm:
-                x = denormalize(x)
-            x = self.prep(x)
-            # print("Shape after prep: ", x.shape)
-        with torch.set_grad_enabled(self.trainable):
-            rel_depth = self.core(x)
-            # print("Output from midas shape", rel_depth.shape)
-            if not self.fetch_features:
-                return rel_depth
-        out = [self.core_out[k] for k in self.layer_names]
+        if denorm:
+            x = denormalize(x)
+        x = self.prep(x)
+        
+        print("Shape after prep: ", x.shape)
+        z = torch.randn((x.shape[0], 512), device=x.device)
 
+
+        x = self.core.pretrained.model.patch_embed(x)
+        x = self.core.pretrained.model.pos_drop(x)
+        print("PATCH EMBED SHAPE", x.shape) #torch.Size([1, 768, 1024])
+        for i in range(0,24):
+            x = self.core.pretrained.model.blocks[i].norm1(x)
+            x = self.core.pretrained.model.blocks[i].attn.qkv(x)
+
+            #Attention Score calculation
+            batch_size, num_patches, feature_dim = x.shape
+            d_k = feature_dim // 3
+            q, k, v = x.reshape(batch_size, num_patches, 3, d_k).split(1, dim=2)
+            q = q.squeeze(2)
+            k = k.squeeze(2)
+            v = v.squeeze(2)
+            attention_scores = torch.matmul(q, k.transpose(-2, -1))
+            attention_scores = attention_scores / np.sqrt(d_k)
+            attention_probs = torch.nn.functional.softmax(attention_scores, dim=-1)
+            x = torch.matmul(attention_probs, v)
+            print("Attention output shape:", x.shape)
+            x = self.core.pretrained.model.blocks[i].attn.attn_drop(x)
+            x = self.core.pretrained.model.blocks[i].attn.proj(x)
+            x = self.core.pretrained.model.blocks[i].attn.proj_drop(x)
+            
+            #Post Attention
+            x = self.core.pretrained.model.blocks[i].drop_path1(x)
+            x = self.core.pretrained.model.blocks[i].norm2(x)
+            x = self.core.pretrained.model.blocks[i].mlp(x)
+            x = self.core.pretrained.model.blocks[i].drop_path2(x)
+            #x.shape = torch.Size([1, 768, 1024])
+            #AdaIN here
+            if i%4 == 0 and i != 0:
+                x = self.adain(x, z) #Adding AdaIN normalization every 4 layers
+                x = x.transpose(1, 2)
+                x = x.squeeze(1)
+            print("SHAPE OUT OF ADAIN", x.shape)
+            print(i,"BLOCK")
+        
+        print("SHAPE AT THE END OF BLOCKS", x.shape)
+        print("FINSHED BLOCKS")
+        unflatten = nn.Sequential(
+            nn.Unflatten(
+                2,
+                torch.Size(
+                    [
+                        384 // self.core.pretrained.model.patch_size[1],
+                        512 // self.core.pretrained.model.patch_size[0],
+                    ]
+                ),
+            )
+        )
+        x = self.core.pretrained.model.norm(x)
+        x = self.core.pretrained.model.fc_norm(x)
+        x = self.core.pretrained.model.head_drop(x)
+        # x = self.core.pretrained.model.head(x)
+        print("Before PostProc", x.shape)
+        print("FINISHED BEIT")
+        #layer_1 = self.act_postprocess1(x)
+        layer_1 = self.act_postprocess1[0:2](x)
+        layer_2 = self.act_postprocess2[0:2](x)
+        layer_3 = self.act_postprocess3[0:2](x)
+        layer_4 = self.act_postprocess4[0:2](x)
+        if layer_1.ndim == 3:
+            layer_1 = unflatten(layer_1)
+        if layer_2.ndim == 3:
+            layer_2 = unflatten(layer_2)
+        if layer_3.ndim == 3:
+            layer_3 = unflatten(layer_3)
+        if layer_4.ndim == 3:
+            layer_4 = unflatten(layer_4)
+        print("UNFLATENNING")
+        layer_1 = self.act_postprocess1[3: len(self.act_postprocess1)](layer_1)
+        layer_2 = self.act_postprocess2[3: len(self.act_postprocess2)](layer_2)
+        layer_3 = self.act_postprocess3[3: len(self.act_postprocess3)](layer_3)
+        layer_4 = self.act_postprocess4[3: len(self.act_postprocess4)](layer_4)
+
+        print("Layer 1 Shape", layer_1.shape)
+
+        print("FINISHED POST PROCESS")
+        layer_1_rn = self.core.scratch.layer1_rn(layer_1)
+        layer_2_rn = self.core.scratch.layer2_rn(layer_2)
+        layer_3_rn = self.core.scratch.layer3_rn(layer_3)
+        layer_4_rn = self.core.scratch.layer4_rn(layer_4)
+        print("FINISHED RN LAYERS")
+        path_4 = self.core.scratch.refinenet4(layer_4_rn)
+        path_3 = self.core.scratch.refinenet3(path_4, layer_3_rn)
+        path_2 = self.core.scratch.refinenet2(path_3, layer_2_rn)
+        path_1 = self.core.scratch.refinenet1(path_2, layer_1_rn)
+        print("FINISHED REFINEMENT")
+        rel_depth = self.core.scratch.output_conv(path_1)
+        print("GOT REL DEPTH")
+        out = [self.core_out[k] for k in self.layer_names]
+        print("CORE OUT")
+        rel_depth = rel_depth.squeeze(1)
+        print("REL DEPTH SHAPE", rel_depth.shape)
+        print("OUT DEPTH", out[0].shape)
         if return_rel_depth:
             return rel_depth, out
         return out
+    
+
+    # def forward(self, x, denorm=False, return_rel_depth=False):
+    #     if denorm:
+    #         x = denormalize(x)
+    #     x = self.prep(x)
+        
+    #     print("Shape after prep: ", x.shape)
+    #     z = torch.randn((1, 512), device=x.device)
+    #     self.core.pretrained.model.patch_size = [16, 16]
+
+    #     x = self.core.pretrained.model.patch_embed(x)
+    #     x = self.core.pretrained.model.pos_drop(x)
+    #     resolution = (x.shape[1], x.shape[2])
+    #     print('RESOLUTION', resolution)
+
+    #     out = []
+    #     for i, hook_idx in enumerate(self.hooks):
+    #         if i < 3:
+    #             layer_output = self.core.pretrained.model(x)
+    #             print("LAYEROUR", layer_output.shape)
+    #             adain_output = self.adains[i](layer_output, z)
+    #             out.append(adain_output)
+    #             x = adain_output  # Passing AdaIN output to the next layer
+    #             resolution = (x.shape[1], x.shape[2])
+    #     rel_depth = self.core.scratch(x)
+    #     out = [self.core_out[k] for k in self.layer_names]
+    #     if return_rel_depth:
+    #         return rel_depth, out
+    #     return out
+
+    # def forward(self, x, denorm=False, return_rel_depth=False):
+    #     with torch.no_grad():
+    #         if denorm:
+    #             x = denormalize(x)
+    #         x = self.prep(x)
+    #     z = torch.randn((1, 512), device=x.device)
+
+    #         #print("Shape after prep: ", x.shape)
+    #     with torch.set_grad_enabled(self.trainable):
+    #         rel_depth = self.core.pretrained.model(x)
+    #         hooks=[0, 4, 8, 11]
+    #         self.core.pretrained.model.blocks[hooks[0]].register_forward_hook(get_activation_BEiT("1"))
+
+    #         print("Output from midas shape", rel_depth.shape)
+    #         if not self.fetch_features:
+    #             return rel_depth
+    #     out = [self.core_out[k] for k in self.layer_names]
+
+    #     if return_rel_depth:
+    #         return rel_depth, out
+    #     return out
+
+    # def forward(self, x, denorm=False, return_rel_depth=False):
+    #     if denorm:
+    #         x = denormalize(x)
+    #     x = self.prep(x)
+    #     print("SHAPE", x.to('cpu').shape)
+    #     # Sample latent code z for AdaIN
+    #     z = torch.randn((1, 512), device=x.device)
+
+    #     # Process through the MiDaS model's encoder, applying AdaIN after each layer
+    #     adain_index = 0
+    #     intermediate_outputs = []
+
+    #     if hasattr(self.core, 'pretrained'):
+    #         x = self.core.pretrained.model.patch_embed(x)
+            
+    #         x = self.core.pretrained.model.pos_drop(x)
+    #         for i, blk in enumerate(self.core.pretrained.model.blocks):
+    #             print("\n NUMBER", i)
+    #             print(x.to('cpu').shape)
+    #             x = blk(x)
+    #             if i < len(self.adain_layers):
+    #                 x = self.adain_layers[i](x, z)
+
+    #         x = self.core.pretrained.norm(x)
+
+    #     for name, module in self.core.named_children():
+            
+    #         x = module(x)
+            
+    #         # Check if the layer is one of the encoder layers where AdaIN should be applied
+    #         if name in self.adain_layers_names:
+    #             x = self.adain_layers[adain_index](x, z)
+    #             adain_index += 1
+            
+    #         # Optionally store intermediate outputs for later use
+    #         if name in self.layer_names:
+    #             intermediate_outputs.append(x)
+
+    #     # Assuming 'x' now holds the output from the last encoder layer after AdaIN application
+    #     # Proceed with the rest of the network (decoder or other components)
+    #     rel_depth = self.core(x)
+
+    #     if not self.fetch_features:
+    #         return rel_depth
+
+    #     out = intermediate_outputs
+
+    #     if return_rel_depth:
+    #         return rel_depth, out
+    #     return out
+
 
     def get_rel_pos_params(self):
         for name, p in self.core.pretrained.named_parameters():
@@ -366,7 +707,7 @@ class MidasCore(nn.Module):
 
 
 nchannels2models = {
-    tuple([256]*5): ["DPT_BEiT_L_384", "DPT_BEiT_L_512", "DPT_BEiT_B_384", "DPT_SwinV2_L_384", "DPT_SwinV2_B_384", "DPT_SwinV2_T_256", "DPT_Large", "DPT_Hybrid"],
+    tuple([256]*5): ["DPT_BEiT_L_384"],
     (512, 256, 128, 64, 64): ["MiDaS_small"]
 }
 
